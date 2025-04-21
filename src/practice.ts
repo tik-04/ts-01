b5ad4848413a702ce0c7c45c33d66095718bcf1d
Nattaphan Pumipak <151860004+tik-04@users.noreply.github.com>
2025-04-21 22:02:47 +0700

    practice 1

--- src/practice.ts
+++ src/practice.ts
@@ -1,33 +1,87 @@
-// 1.
-// function identity<T>(arg: T): T {
-//     return arg;
-// }
+// 1.Type Alias , Interface
+type Person = {
+    name: string,
+    age: number
+};
+
+const person1: Person = {name:"tik",age:18}
+
+interface Person2 {
+    name: string,
+    age: number
+}
+
+const person2: Person2 = {name:"tik2",age:20}
+
+console.log(person1,person2)
+
+// 2.Narrowing
+
+function formatInput(arg:string): string;
+function formatInput(arg:number): string;
+function formatInput(arg:boolean): string;
+function formatInput(arg: string | number | boolean): string {
+    if (typeof arg === "string") {
+        return arg.toUpperCase();
+    }
+    if (typeof arg === "number") {
+        let ans = arg.toString() + " points"
+        return ans
+    }
+    if (typeof arg === "boolean") {
+        return arg ? "Yes" : "No";
+    }
+    throw new Error("Invalid agrument type");
+}
+
+// 3. Generic
+function flipArray<T>(arr: T[]): T[] {
+    let new_arr = []
+    for (let i = arr.length - 1; i >= 0; i--){
+        new_arr.push(arr[i])
+    }
+    return new_arr
+}
+
+console.log(flipArray([1, 2, 3]))
+
+// 4. Utility
+type Profile = {
+    name: string;
+    email: string;
+    age: number;
+    phone?: string;
+};
+
+type ReadonlyUser = Readonly<Profile>;
+type ProfileWithoutAge = Omit<Profile, 'age'>;
+type ProfileWithOnlyContact = Pick<Profile, 'email' | 'phone' >;  
+
+
+//5. class
+
+class Account {
+    constructor(public owner:string,public balance:number) {
+        this.owner = owner
+        this.balance = balance
+    }
+    deposit(amount: number) : void { this.balance += amount }
+    withdraw(amount: number): void {
+        if (this.balance >= amount) {
+            this.balance -= amount
+        }
+        throw new Error("You not have enoght money")
+    }
+    show(): void { console.log(`${this.owner} balance: ${this.balance}`)}
+}
+
+class AdminAccount extends Account {
+    constructor(owner:string,balance:number,public role:string) {
+        super(owner,balance)
+    }
+    override show(): void {console.log(`${this.owner} balance: ${this.balance} role: ${this.role}`)}
+
+}
   
-// console.log(identity<string>("tik"))
-// console.log(identity<number>(99))
 
-// 2.
-// function swap<T, U>(a: [T, U]): [U, T] {
-//     return [a[1],a[0]]
-// }
-console.log("test")
-// 3.
 
-// function logArray<T>(arr: T[]): void {
-//     for (let i = 0; i < arr.length; i++) {
-//         console.log(arr[i])
-//     }
-// }
-
-// 4.
-// function fetchProp<T, K extends keyof T>(obj: T, key: K): T[K] {
-//     return obj[key]
-// }
-
-// const profile = { name: "tik", age: 20 };
-// console.log(fetchProp(profile,"name"))
-
-// 5.
-// function doubleLength<T extends { length: number }>(val: T): number {
-//     return val.length ** 2
-// }
